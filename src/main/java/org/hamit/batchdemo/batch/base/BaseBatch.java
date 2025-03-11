6c2c65382127de1d68f8c67dba27ea016f92bce6
De3ph <05190000108@ogrenci.ege.edu.tr>
2025-03-11 16:40:26 +0300

    feat: implement initalizing bean

--- src/main/java/org/hamit/batchdemo/batch/base/BaseBatch.java
+++ src/main/java/org/hamit/batchdemo/batch/base/BaseBatch.java
@@ -1,14 +1,23 @@
 package org.hamit.batchdemo.batch.base;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 
+@Slf4j
 @Configuration
-public abstract class BaseBatch {
+public abstract class BaseBatch implements InitializingBean {
     @Bean
     public PlatformTransactionManager transactionManager() {
         return new JpaTransactionManager();
     }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        log.info("Batch initialized : {}", this.getClass().getSimpleName());
+    }
+
 }
